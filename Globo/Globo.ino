80d259cc5adb9638a1431c3f386de35a2e56afde
joseatienzapiedra <joseatienzapiedra@gmail.com>
2016-11-16 21:43:30 +0000

    Cambio en la cabecera

--- Globo/Globo.ino
+++ Globo/Globo.ino
@@ -1,4 +1,25 @@
-//Prueba de actualizacion 2
+/**
+ * Copyright (c) 2016 Jose Atienza  <joseatienzapiedra@gmail.com> http://www.joseatienza.com/
+ *                    Juan J. Pardo <dejpardo@gmail.com>
+ * All rights reserved.
+ *
+ * This file is part of SondaI_Arduino.
+ * See:  https://github.com/joseatienzapiedra/SondaI_Arduino
+ *
+ * SondaI_Arduino is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SondaI_Arduino is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PicaMesh-server.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 
 #include <max6675.h>
 #include <LiquidCrystal_I2C.h>
